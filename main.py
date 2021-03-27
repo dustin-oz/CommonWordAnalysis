ef039c89f7a79391d63bf120b3c3f0b7747674c3
Dustin <42046575+dustin-oz@users.noreply.github.com>
2021-03-26 21:50:45 -0500

    Added some commenting for readability.

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import collections
 
-file = open('txt_dump.txt', encoding="utf8")
+file = open('txt_dump.txt', encoding="utf8")  # File containing text to analyze
 
 a = file.read()
 
+# File containing any words to exclude from analysis i.e. mr, mrs.
 stopwords = set(line.strip() for line in open('txt_blacklist.txt'))
 stopwords = stopwords.union(
-    set(['mr', 'mrs', 'one', 'two', 'said', 'the', 'a', 'and', 'to', 'of']))
+    set(['mr', 'mrs', 'one', 'two', 'said', 'the', 'a', 'and', 'to', 'of']))  # Hardcoded a few constants that I want to exclude always.
 
 wordcount = {}
 
@@ -23,6 +24,7 @@ for word in a.lower().split():
         else:
             wordcount[word] += 1
 
+# How many keywords to search for in txt_dump.txt.
 num_print = int(input("Number of words to find: "))
 
 print("\nAnalysis Complete.\n\nThe {} most common words found:\n".format(num_print))
